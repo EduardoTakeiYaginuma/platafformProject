0af54256563b7f5bed9041eff531e97eb7a41029
EduardoTakeiYaginuma <eduyagnuma10@gmail.com>
2025-04-30 14:42:08 -0300

    dev

---
+++ api/product/src/main/java/store/account/AccountIn.java
@@ -0,0 +1,12 @@
+package store.account;
+import lombok.Builder;
+import lombok.experimental.Accessors;
+
+@Builder @Accessors(fluent = true)
+public record AccountIn(
+    String name,
+    String email,
+    String password
+) {
+    
+}
